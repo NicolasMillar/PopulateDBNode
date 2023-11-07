1ce07144da34a42465dddf59937223d6a603e8d1
Nicolas <nicolas.millar.millar@gmail.com>
2023-11-07 18:07:20 -0300

    lee el archivo de texto con las traducciones

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ const db = pgp(connectionString);
 
 db.connect()
   .then(() => {
-    console.log('Conexión exitosa a la base de datos');
+    const fs = require('fs');
+    fs.readFile('db.txt', 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error al leer el archivo db.txt:', err);
+        return;
+      }
+
+      // Divide el contenido en líneas
+      const lines = data.split('\n');
+
+      // Procesa cada línea para obtener las palabras de origen y sus traducciones
+      lines.forEach((line) => {
+        const [origen, traducciones] = line.split('-');
+
+        // Divide las traducciones y palabras de origen en palabras individuales
+        const palabrasOriginales = origen.split(',');
+        const traduccionesIndividuales = traducciones.split(',');
+        palabrasOriginales.forEach(palabraOriginal => {
+          traduccionesIndividuales.forEach(traduccion => {
+            console.log(palabraOriginal + "-" + traduccion + "\n");
+          });
+        });
+      });
+    });
   })
   .catch((error) => {
     console.error('Error al conectar a la base de datos:', error);
